bcfdcd5d12b44df613f73eef67bc00a7c7697b5d
pablo martinez <pablomartinezsepulveda@hotmail.com>
2021-08-02 14:52:23 -0400

    comienzo de controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: "Acogedor Loft",
+  address:" Santiago, Región Metropolitana",
+  description:"3 huéspedes, · 1 habitación, · 2 camas, · 1 baño",
+  price_per_night: 18.571,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: "Apartamento Confortable",
+  address: "Providencia, Región Metropolitana, Chile",
+  description:" 1 huéspedes, · 1 habitación, · 1 camas, · 1 baño",
+  price_per_night: 18.000,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: "San Isidro",
+  address: "Santiago, Región Metropolitana, Chile",
+  description:"2 huéspedes, · 1 habitación, · 1 camas, · 1 baño",
+  price_per_night: 20.000,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: "Flat in heart of Barrio Lastarria",
+  address:"Santiago, Región Metropolitana, Chile",
+  description: "2 huéspedes, · 1 habitación, · 1 camas, · 1 baño",
+  price_per_night: 18.000,
+  number_of_guests: 2
+)
